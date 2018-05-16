f99fda03c9bdf2d978df278ddd41b2a390cacd3f
Nicolas Fournier <nicolas.fournier.2@umontreal.ca>
2018-05-16 16:54:51 -0400

    New folder and index for resources

--- components/TopNavbar.vue
+++ components/TopNavbar.vue
@@ -8,31 +8,54 @@
         <b-nav-item to="/">Accueil</b-nav-item>
         <!-- Navbar dropdowns -->
         <b-nav-item-dropdown text="Parcours pédagogique" right>
+          <b-dropdown-header>Chapitre 1 "Initiation"</b-dropdown-header>
           <b-dropdown-item to="/S01/S01-index">Semaine 1</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-header>Chapitre 2 "Cadre spatio-temporel"</b-dropdown-header>
           <b-dropdown-item to="/S02/S02-index">Semaine 02</b-dropdown-item>
           <b-dropdown-item href="#">Semaine 03</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-header>Chapitre 3 "Orientations thématiques"</b-dropdown-header>
           <b-dropdown-item href="#">Semaine 04</b-dropdown-item>
           <b-dropdown-item  >Semaine 05</b-dropdown-item>
           <b-dropdown-item  >Semaine 06</b-dropdown-item>
           <b-dropdown-item  >Semaine 07</b-dropdown-item>
           <b-dropdown-item  >Semaine 08</b-dropdown-item>
           <b-dropdown-item  >Semaine 09</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-header>Chapitre 4 "Projet de recherche"</b-dropdown-header>
           <b-dropdown-item >Semaine 10</b-dropdown-item>
           <b-dropdown-item >Semaine 11</b-dropdown-item>
           <b-dropdown-item >Semaine 12</b-dropdown-item>
           <b-dropdown-item >Semaine 13</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-header>Conclusions</b-dropdown-header>
           <b-dropdown-item >Semaine 14</b-dropdown-item>
           <b-dropdown-item >Semaine 15</b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item-dropdown text="Ressources" right>
-          <b-dropdown-header>Orientation et thématiques</b-dropdown-header>
+          <b-dropdown-item  >Initiation au haut Moyen Âge</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-header>Spatio-temporel</b-dropdown-header>
+          <b-dropdown-item  >Byzance, les Juifs et le monde musulman</b-dropdown-item>
+          <b-dropdown-item  >Italie</b-dropdown-item>
+          <b-dropdown-item  >Péninsule Ibérique</b-dropdown-item>
+          <b-dropdown-item  >Royaumes des Francs</b-dropdown-item>
+          <b-dropdown-item  >Francie occidentale – Francie</b-dropdown-item>
+          <b-dropdown-item  >Francie orientale – Germanie</b-dropdown-item>
+          <b-dropdown-item  >Îles Britanniques</b-dropdown-item>
+          <b-dropdown-item  >Scandinavie</b-dropdown-item>
+          <b-dropdown-item  >Europe de l’Est</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-header>Orientation thématiques</b-dropdown-header>
           <b-dropdown-item href="#">Politique et droit</b-dropdown-item>
           <b-dropdown-item to="">Société et économie</b-dropdown-item>
           <b-dropdown-item href="#">Église et religion</b-dropdown-item>
           <b-dropdown-item href="#">Culture et mentalités</b-dropdown-item>
-          <b-dropdown-item  >Les sciences auxiliaires de l’histoire</b-dropdown-item>
           <b-dropdown-divider></b-dropdown-divider>
-          <b-dropdown-header>Autre</b-dropdown-header>
+          <b-dropdown-item  >Sources</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item  >Ressources méthodologiques</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
---
+++ pages/Ressources/R01-index.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <b-jumbotron 
+            bg-variant="https://farm6.staticflickr.com/5564/30027314480_81db2b8384_b.jpg" 
+            header="Spacio-temporel" 
+            lead="" 
+            text-variant="white">
+        </b-jumbotron>
+        <b-container class="">
+            <b-row  align-h="end">
+                <b-col>
+                    <h4 class="alert-heading">Cartes historiques</h4>
+                    <p  class="lead">
+                    <hr>
+                    <p> 
+                    </p>
+                    <b-tabs >
+                        <b-tab title="Europe et cartes globales" active>
+                        </b-tab>
+                        <b-tab title="Empire romain tardif et le temps des migrations" >
+                        </b-tab>
+                        <b-tab title="Byzance, les Juifs et le monde musulman">
+                            <b-card-group deck>
+                                <b-card v-b-modal.modal1 img-alt="Card image" img-top>
+                                    <p class="card-text">
+                                        Plan de cours
+                                    </p>
+                                </b-card>
+                                <b-card v-b-modal.modal1 img-alt="Card image" img-top>
+                                    <p class="card-text">
+                                        Fiches descriptives des évaluations
+                                    </p>
+                                </b-card>
+                                <b-card v-b-modal.modal1 img-alt="Card image" img-top>
+                                    <p class="card-text">
+                                        Guide du participant à une classe virtuelle Adobe Connect
+                                    </p>
+                                </b-card>
+                            </b-card-group>
+                        </b-tab>
+                        <b-tab title="Italie" >
+                        </b-tab>
+                        <b-tab title="Péninsule Ibérique" >
+                        </b-tab>
+                        <b-tab title="Royaumes des Francs" >
+                        </b-tab>
+                        <b-tab title="Francie occidentale – Francie" >
+                        </b-tab>
+                        <b-tab title="Francie orientale – Germanie" >
+                        </b-tab>
+                        <b-tab title="Îles Britanniques" >
+                        </b-tab>
+                        <b-tab title="Scandinavie" >
+                        </b-tab>
+                        <b-tab title="Europe de l’Est" >
+                        </b-tab>
+                    </b-tabs>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+    layout: 'default',
+    components: {
+        Modal
+        },
+        data () {
+        return {
+            msg: 'semaine 2'//,
+            //isActive: true
+        }
+    }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.jumbotron {
+  background: url(https://farm9.staticflickr.com/8267/29693441973_edba460a5c_b.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
